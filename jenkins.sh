a247f522039db5f698be897260887eaf52a164b1
Mark H. Butler <markhenrybutler@gmail.com>
2015-01-10 15:02:58 -0800

    Want to get docker to start in container

--- jenkins.sh
+++ jenkins.sh
@@ -25,6 +25,7 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
 fi
 
+docker -d &
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
 exec "$@"
 
